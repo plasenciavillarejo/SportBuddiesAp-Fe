70b978d497646815eca07029a3652708a300b1b1
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-09-12 14:29:06 +0200

    [Plasencia] - [Inicio Angular con Bootstrap]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // Importamos los componentes "Login"
+  imports: [LoginComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
