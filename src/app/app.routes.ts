114fcb483b7e00ec9b7288c3e2e55a54117e13bc
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-09-17 10:55:31 +0200

    su

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { NuevaActividadComponent } from './components/nueva-actividad/nueva-actividad.component';
 
 export const routes: Routes = [
     /**Por defecto si el path es vació nos redigira a usuarios */
@@ -16,5 +17,9 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
-      } 
+    },
+    {
+        path: 'nueva-actividad',
+        component: NuevaActividadComponent
+    } 
 ];
---
+++ src/app/components/nueva-actividad/nueva-actividad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-actividad',
+  standalone: true,
+  imports: [],
+  templateUrl: './nueva-actividad.component.html',
+  styleUrl: './nueva-actividad.component.css'
+})
+export class NuevaActividadComponent {
+
+}
