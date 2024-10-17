429fa1490f4ea5d611ef632b604ac79c664a654e
MANIVAAN <a.nieto@ibermatica.com>
2024-10-17 12:45:05 +0200

    [Andres] [ Historial reservas y mis reservas]

--- src/app/components/historial-reservas/historial-reservas.component.ts
+++ src/app/components/historial-reservas/historial-reservas.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Reserva } from '../../models/reserva';
+import { ReservasService } from '../../services/reservas.service';
+import { ReservasResponse } from '../../models/reservasResponse';
+import { TokenService } from '../../services/token.service';
 @Component({
   selector: 'app-historial-reservas',
   standalone: true,
@@ -8,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrl: './historial-reservas.component.css'
 })
 export class HistorialReservasComponent {
+
+  titulo: string = 'Historial de reservas';
+  constructor(private reservaService: ReservasService,
+    private tokenService: TokenService
+  ) {}
+
+  listReservasResponse: ReservasResponse[] = [];
+
+  ngOnInit(): void {
+    this.listReservationHistory(this.tokenService.obtainIdUser());
+  }
+
+  /**
+   * Función encargada de listar las reservas que contiene un usuario
+   * @param idUsuario 
+   */
+  listReservationHistory(idUsuario: number): void {
+    this.reservaService.consultReservationsHistory(idUsuario).subscribe({
+      next: response => {
+        this.listReservasResponse = response;
+      }
+    })
+  }
 
 }
