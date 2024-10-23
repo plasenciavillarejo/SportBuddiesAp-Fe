55865389b0abe5a611f3649aea75411b2c7f9af3
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-10-23 16:42:39 +0200

    Logica para cancelar una reserva y devolver el dinero

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -302,7 +302,8 @@ export class UsuariosComponent implements OnInit {
   formatDate(): string {
     const date: Date = new Date();
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Los meses van de 0 a 11
+    // Los meses van de 0 a 11
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     
     return `${year}-${month}-${day}`;
