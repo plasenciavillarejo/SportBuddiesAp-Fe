0d658b0f3b4b6c2db03a664a80e95ebf0b1f5de5
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-11-11 14:13:45 +0100

    Validacion formularios reactivos

--- src/app/components/nuevo-usuario/nuevo-usuario.component.ts
+++ src/app/components/nuevo-usuario/nuevo-usuario.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../models/usuario';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
 import Swal from 'sweetalert2';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,7 +10,7 @@ declare var bootstrap: any
 @Component({
   selector: 'app-nuevo-usuario',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './nuevo-usuario.component.html',
   styleUrl: './nuevo-usuario.component.css'
 })
@@ -20,29 +20,32 @@ export class NuevoUsuarioComponent implements OnInit {
 
   errorPassword!: boolean;
   idUsuario!: number;
-  formSubmit!: boolean;
   passwordRequired!: boolean;
 
-  form: FormGroup;
+  validateForm!: FormGroup
+  formSubmit!: boolean;
 
   listaProvincias: any[] = [];
   listaMunicipos: string[] = []
 
+  nacionalidadesEuropeas = [
+    { codigo: 'ES', pais: 'España' },
+    { codigo: 'IT', pais: 'Italia' },
+    { codigo: 'FR', pais: 'Francia' },
+    { codigo: 'DE', pais: 'Alemania' },
+    { codigo: 'GB', pais: 'Reino Unido' },
+    { codigo: 'PT', pais: 'Portugal' },
+    { codigo: 'NL', pais: 'Países Bajos' },
+    { codigo: 'BE', pais: 'Bélgica' },
+    { codigo: 'SE', pais: 'Suecia' },
+    { codigo: 'PL', pais: 'Polonia' }
+  ];
+  
   constructor(private usuarioService: UsuarioService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private formBuilder: FormBuilder
-  ) { 
-    this.form = this.formBuilder.group({
-      nombreUsuario: ['', Validators.required], // Campo obligatorio
-      email: ['', [Validators.required, Validators.email]], 
-      direccion: ['', Validators.required],
-      provincia: ['', Validators.required],
-      municipio: ['', Validators.required],
-      codigoPostal: ['', Validators.required],
-      pais: ['', Validators.required],
-      numeroTelefono: ['', Validators.required]
-    });
+    private formBuilder: FormBuilder,
+  ) {
   }
 
   ngOnInit(): void {
@@ -50,10 +53,13 @@ export class NuevoUsuarioComponent implements OnInit {
     this.activatedRoute.params.subscribe(params => {
       this.idUsuario = params['idUsuario'];
       if (this.idUsuario != null) {
+        // Inicializamos la validacion de angular con los formularios reactivos
+        this.initFormReactiveComplet();
         this.obtainUserComplet(this.idUsuario);
+      } else {
+        this.initFormReactiveNewUser();
       }
     });
-    this.loadProvince();
   }
 
   /**
@@ -78,18 +84,20 @@ export class NuevoUsuarioComponent implements OnInit {
    * Función encargada de crear un nuevo usuario para acceder a la aplicación que retornara a la página de inicio una vez que ha sido creado
    * @param usuario 
    */
-  createOrUpdateUser(usuario: Usuario) {
+  createOrUpdateUser() {
     this.formSubmit = true;
-    
+
     // Validacion formulario reactivo
-    if(this.form.invalid) {
-      // En caso de que haya algun error, bloqueams el envio
+    if (this.idUsuario == null && this.validateForm.invalid && this.validarPassword(this.validateForm)) {
+      return;
+    } else if (this.idUsuario != null && this.validateForm.invalid) {
       return;
     }
+    // Transformamos el formulario reactivo en un objeto usuario
+    const usuarioData: Usuario = this.validateForm.value as Usuario;
 
-    this.validarCampos(usuario);
     if (this.idUsuario === undefined) {
-      this.usuarioService.createUser(usuario).subscribe({
+      this.usuarioService.createUser(usuarioData).subscribe({
         next: response => {
           Swal.fire(
             'Usuario creado exitosamente',
@@ -116,7 +124,7 @@ export class NuevoUsuarioComponent implements OnInit {
         confirmButtonText: "Sí, actualizar!"
       }).then((result) => {
         if (result.isConfirmed) {
-          this.usuarioService.updateUser(usuario).subscribe({
+          this.usuarioService.updateUser(usuarioData).subscribe({
             next: response => {
               Swal.fire(
                 'Usuario actualizao exitosamente',
@@ -136,31 +144,46 @@ export class NuevoUsuarioComponent implements OnInit {
    * Función encargada de validar los criterios de la contraseña
    * @param usuario 
    */
-  private validarCampos(usuario: Usuario): void {
-    this.errorPassword = usuario.password === undefined || usuario.password.trim() === '';
+  private validarPassword(validateForm: FormGroup): boolean {
+    this.errorPassword = validateForm.get('password')?.value === undefined || validateForm.get('password')?.value.trim() === ''; 
     if(this.idUsuario === undefined) {
-      if (!this.validateCriteriaPassword(this.usuario.password) || this.errorPassword) {
-        throw new Error;
+      if (!this.validateCriteriaPassword(validateForm.get('password')?.value) || this.errorPassword) {
+        return true;
       }
     }    
+    return false;
   }
 
   /**
-   * Función encargada de recibir un idUsuario y cargar el usuario completo
+   * Función encargada de recibir un idUsuario y cargar el usuario completo cuando el usuario esta registrado en la aplicación
    * @param idUsuario 
    * @returns 
    */
-  private obtainUserComplet(idUsuario: number): Usuario {
+  private obtainUserComplet(idUsuario: number): void {
     this.usuarioService.obtainUserDto(idUsuario).subscribe({
       next: response => {
         if (response != null) {
           this.usuario = response;
+          this.validateForm.patchValue({
+            idUsuario: idUsuario,
+            nombreUsuario: this.usuario.nombreUsuario,
+            email: this.usuario.email,
+            direccion: this.usuario.direccion,
+            provincia: this.usuario.provincia,
+            municipio: this.usuario.municipio,
+            codigoPostal: this.usuario.codigoPostal,
+            pais: this.usuario.pais,
+            numeroTelefono: this.usuario.numeroTelefono
+          });
         }
+        // En los formularios Reactivos, el disabled lo hace typescrypt y no en el html
+        this.validateForm.get('nombreUsuario')?.disable();
+        this.loadProvince();
+        this.loadMunicipaliti(this.usuario.provincia);
       }, error: error => {
         throw new error;
       }
     });
-    return this.usuario;
   }
 
   /**
@@ -216,12 +239,12 @@ export class NuevoUsuarioComponent implements OnInit {
     });
   }
 
-    /**
+  /**
    * Función encargada de cargar los municipos asociados a una provincia
    * @param event 
    */
     loadMunicipaliti(event: any): void {
-      const municipio: string = event.target.value.toString();
+      const municipio: string = event.target === undefined ? event :  event.target.value.toString();
       this.usuarioService.loadMunic(municipio).subscribe({
         next: (response) => {
           this.listaMunicipos = response;
@@ -230,5 +253,40 @@ export class NuevoUsuarioComponent implements OnInit {
         }
       });
     }
+
+    /**
+     * Función global para inicializar el formulario reactivo sin datos.
+     * @param usuario 
+     */
+  private initFormReactiveComplet(): void {
+    // Inicializamos la validacion de angular con los formularios reactivos
+    this.validateForm = this.formBuilder.group({
+      idUsuario: [''],
+      nombreUsuario: [''],
+      email: ['', [Validators.required, Validators.email]],
+      direccion: ['', [Validators.required]],
+      provincia: ['', [Validators.required]],
+      municipio: ['', [Validators.required]],
+      codigoPostal: ['', [Validators.required, Validators.pattern('[0-9]{5}$')]],
+      pais: ['', [Validators.required]],
+      numeroTelefono: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]]
+    });
+  }
+
+  private initFormReactiveNewUser(): void {
+    // Inicializamos la validacion de angular con los formularios reactivos
+    this.validateForm = this.formBuilder.group({
+      nombreUsuario: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+
+  private noWhiteSpaceValidator(control: AbstractControl): ValidationErrors | null {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+}
 
 }
