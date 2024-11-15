d2477dbfb725cfec6ec72ecabdea2aae18bc5fca
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-11-15 12:45:42 +0100

    Optimización código

--- src/app/core/interceptor/error.interceptor.ts
+++ src/app/core/interceptor/error.interceptor.ts
@@ -1,19 +1,26 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 import Swal from 'sweetalert2';
+import { inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService =  inject(AuthService);
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {    
       // Con esta exclusión, evitamos la carga inicial de los combos que de error en caso de que falte la conexíon de algún componente 'error.status !== 0'  
       if(error.status !== 200 && error.status !== 0 && error.status !== 503){
+        if(error.status === 401) {
+          authService.logout();
+        } else {
         Swal.fire(
           'Error',
           error.error.mensaje, 
           'error'
         )        
-        return throwError(() => error);
-      } else {
+      }
+        return throwError(() => error); 
+      } else  {
         return next(req);
       }
     })
