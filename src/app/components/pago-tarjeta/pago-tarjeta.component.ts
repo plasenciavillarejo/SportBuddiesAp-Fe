34fda4e45ee9582425e4a521db21a4512795eefc
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2024-11-23 16:11:37 +0100

    Logica confirmar-asistencia

--- src/app/components/pago-tarjeta/pago-tarjeta.component.ts
+++ src/app/components/pago-tarjeta/pago-tarjeta.component.ts
@@ -9,6 +9,8 @@ import { SpinnerModalComponent } from '../spinner-modal/spinner-modal.component'
 import { environment } from '../../../environments/environment';
 import { TokenService } from '../../services/token.service';
 import Swal from 'sweetalert2';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-pago-tarjeta',
@@ -20,6 +22,7 @@ import Swal from 'sweetalert2';
 export class PagoTarjetaComponent implements OnInit {
 
   pagoTarjetaRequest: PagoTarjetaRequest = new PagoTarjetaRequest();
+  usuario: Usuario = new Usuario();
 
   // PLASENCIA - CAMBIAR LUEGO A VARIABLES ENVIRONMENT
   stripe: Stripe | null = null;
@@ -36,7 +39,8 @@ export class PagoTarjetaComponent implements OnInit {
     private pagoTarjetaService: PagoTarjetaService,
     private servicioCompartido: ServicioCompartidoService,
     private tokenService: TokenService,
-    private router: Router) { }
+    private router: Router,
+    private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -69,51 +73,59 @@ export class PagoTarjetaComponent implements OnInit {
       return;
     }
 
-    this.stripe.createPaymentMethod({
-      type: 'card',
-      card: this.cardElement,
-      billing_details: {
-        name: pagoTarjetaRequest.nombreTitular,
-        phone: pagoTarjetaRequest.nombreTitular,
-        email: 'plasenciavillarejo@gmail.com',
-        address: {
-          line1: 'Calle Nueva', // Línea principal de la dirección.
-          city: 'Ciudad Ejemplo', // Ciudad.
-          postal_code: '12345', // Código postal.
-          country: 'ES' // Código de país en formato ISO (como 'ES' para España).
-        }
-      },
-    }).then((result) => {
-      if (result.error) {
-        console.error(result.error.message);
-      } else {
-        this.servicioCompartido.showSpinnerModal();
-        console.log('PaymentMethod created:', result.paymentMethod);        
-        pagoTarjetaRequest.metodoPago = result.paymentMethod.id;
-        pagoTarjetaRequest.cantidad = this.precioActividad;
-        pagoTarjetaRequest.divisa = 'EUR';
-        pagoTarjetaRequest.descripcion = 'Pago Pista ' + this.nombreActividad;
-        pagoTarjetaRequest.idUsuario = this.tokenService.obtainIdUser();
-        pagoTarjetaRequest.idReservaUsuario = this.idReservaUsuario;
-        
-        this.pagoTarjetaService.paymentCard(pagoTarjetaRequest).subscribe({
-          next: response => {
-            if (response.clientSecret != null) {
-              this.servicioCompartido.hideSpinnerModal();
-              Swal.fire(
-                'Pago confirmado',
-                'Se ha realizado el pago exitosamente',
-                'success'
-              );
-              this.router.navigate(['/usuarios'], { replaceUrl: true });
+    // Obtenemos los datos del usuario necesario para rellenar los detalles de la tarjeta
+    this.usuarioService.obtainUserDto(this.tokenService.obtainIdUser()).subscribe({
+      next: (response) => {
+        if(response != null){
+          this.usuario = response;
+          
+          this.stripe?.createPaymentMethod({
+            type: 'card',
+            card: this.cardElement,
+            billing_details: {
+              name: pagoTarjetaRequest.nombreTitular,
+              phone: this.usuario.numeroTelefono,
+              email: this.usuario.email,
+              address: {
+                line1: this.usuario.direccion,
+                city: this.usuario.provincia,
+                postal_code: this.usuario.codigoPostal,
+                country: 'ES'
+              }
+            },
+          }).then((result) => {
+            if (result.error) {
+              console.error(result.error.message);
+            } else {
+              this.servicioCompartido.showSpinnerModal();
+              console.log('PaymentMethod created:', result.paymentMethod);        
+              pagoTarjetaRequest.metodoPago = result.paymentMethod.id;
+              pagoTarjetaRequest.cantidad = this.precioActividad;
+              pagoTarjetaRequest.divisa = 'EUR';
+              pagoTarjetaRequest.descripcion = 'Pago Pista ' + this.nombreActividad;
+              pagoTarjetaRequest.idUsuario = this.tokenService.obtainIdUser();
+              pagoTarjetaRequest.idReservaUsuario = this.idReservaUsuario;
+              
+              this.pagoTarjetaService.paymentCard(pagoTarjetaRequest).subscribe({
+                next: response => {
+                  if (response.clientSecret != null) {
+                    this.servicioCompartido.hideSpinnerModal();
+                    Swal.fire(
+                      'Pago confirmado',
+                      'Se ha realizado el pago exitosamente',
+                      'success'
+                    );
+                    this.router.navigate(['/usuarios'], { replaceUrl: true });
+                  }
+                }, error: error => {
+                  this.servicioCompartido.hideSpinnerModal();
+                  throw new error;
+                }
+              });
             }
-          }, error: error => {
-            this.servicioCompartido.hideSpinnerModal();
-            throw new error;
-          }
-        });
+          });
+        }
       }
-    });
+    })
   }
-
 }
