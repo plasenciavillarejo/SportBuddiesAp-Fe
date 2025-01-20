3537f6d05736631c98084004e546db862f6efe87
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2025-01-20 20:59:32 +0100

    Configuracion dominio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    authorize_uri: 'http://200.234.230.76:9000/oauth2/authorize?',
+    authorize_uri: 'http://www.sportbuddies.es:9000/oauth2/authorize?',
     client_id: 'client-angular',
-    redirect_uri: 'http://200.234.230.76:4200/authorize',
+    redirect_uri: 'http://www.sportbuddies.es:4200/authorize',
     scope: 'openid profile',
     response_type: 'code',
     response_mode: 'form_post',
@@ -10,13 +10,13 @@ export const environment = {
     /* Con el Mock
     code_verifier: 'lJoczNsB9lCJ2hVG174AtaXjYcIxLxbIVQ2La7Yxk4d',
     code_challenge: '5t796pVGz5Nq3tPIfZo2XCAq28_Hc8QCWJqwY_Po7LQ',*/
-    token_url: 'http://200.234.230.76:9000/oauth2/token',
+    token_url: 'http://www.sportbuddies.es:9000/oauth2/token',
     grant_type: 'authorization_code',
-    logout_url: 'http://200.234.230.76:8090/logout',
+    logout_url: 'http://www.sportbuddies.es:8090/logout',
     secret_pkce: '12345',
-    hostname_port_local_gtw: 'http://200.234.230.76:8090',
-    hostname_port_local_fe: 'http://200.234.230.76:4200',
-    hostname_port_local_oauth: 'http://200.234.230.76:9000',
+    hostname_port_local_gtw: 'http://www.sportbuddies.es:8090',
+    hostname_port_local_fe: 'http://www.sportbuddies.es:4200',
+    hostname_port_local_oauth: 'http://www.sportbuddies.es:9000',
     key_stripe: 'pk_test_51QHLE5KPTU50YCkCu2PCHKCHyjTj0tgu0oUvbvQQQCLiDJkiw0xoaGmJ1rDiV7glZvtbxzIbyn30LAOwn5IVhJ8f008GoACgYJ',
     username_cobardo: 'pro-1962508930052348487',
     secret_id_cobardo: 'corbado1_gTYNCwuJSxGw3dmAnyDGjJAwLnYANK'
